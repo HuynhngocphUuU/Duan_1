869b0ae6ba7c15211d181eb292dbb4294fe30e5a
Huaquyen037205 <huaq2961@gmail.com>
2024-11-25 20:17:43 +0700

    first commit

---
+++ Mvc/View/product.php
@@ -0,0 +1,235 @@
+<body>
+    <div class="nen">
+        <div id="tk">
+            <nav class="container">
+                <ul id="main-menu">
+                    <li><a href="">Tìm kiếm theo loại</a>
+                        <ul class="sub-menu">
+                            <li><a href="">Áo Sơ Mi</a></li>
+                            <li><a href="">Áo Polo</a></li>
+                            <li><a href="">Áo Khoác</a></li>
+                            <li><a href="">Quần Jean</a></li>
+                            <li><a href="">Quần Tây</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="">Tìm kiếm theo giá</a>
+                        <ul class="sub-menu">
+                            <li><a href="">Giá dưới 500</a></li>
+                            <li><a href="">Từ 500 - 1 triệu</a></li>
+                            <li><a href="">Từ 1 triệu - 2 triệu</a></li>
+                            <li><a href="">Từ 2 triệu - 3 triệu</a></li>
+                            <li><a href="">Giá trên 3 triệu</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+        <ul class="products">
+            <li>
+                <div class="product-item">
+                    <div class="product-top">
+                        <a href="" class="product-thumb">
+                            <img src="img/1.jpg" alt="">
+                        </a>
+                        <a href="" class="mn">Mua ngay</a>
+                    </div>                   
+                    <div class="product-info">
+                        <a href="" class="product-name">City Classic</a>
+                        <div class="gia" >
+                            <div class="price">400.000đ</div>
+                            <div class="priceg">1.400.000đ</div>
+                        </div>
+                    </div>
+                </div>
+            </li>
+            <li>
+                <div class="product-item">
+                    <div class="product-top">
+                        <a href="" class="product-thumb">
+                            <img src="img/2.jpg" alt="">
+                        </a>
+                        <a href="" class="mn">Mua ngay</a>
+                    </div>                   
+                    <div class="product-info">
+                        <a href="" class="product-name">City Classic</a>
+                        <div class="gia" >
+                            <div class="price">400.000đ</div>
+                            <div class="priceg">1.400.000đ</div>
+                        </div>
+                    </div>
+                </div>
+            </li>
+            <li>
+                <div class="product-item">
+                    <div class="product-top">
+                        <a href="" class="product-thumb">
+                            <img src="img/3.jpg" alt="">
+                        </a>
+                        <a href="" class="mn">Mua ngay</a>
+</div>                   
+                    <div class="product-info">
+                        <a href="" class="product-name">City Classic</a>
+                        <div class="gia" >
+                            <div class="price">400.000đ</div>
+                            <div class="priceg">1.400.000đ</div>
+                        </div>
+                    </div>
+                </div>
+            </li>
+            <li>
+                <div class="product-item">
+                    <div class="product-top">
+                        <a href="" class="product-thumb">
+                            <img src="img/4.jpg" alt="">
+                        </a>
+                        <a href="" class="mn">Mua ngay</a>
+                    </div>                   
+                    <div class="product-info">
+                        <a href="" class="product-name">City Classic</a>
+                        <div class="gia" >
+                            <div class="price">400.000đ</div>
+                            <div class="priceg">1.400.000đ</div>
+                        </div>
+                    </div>
+                </div>
+            </li>
+            <li>
+                <div class="product-item">
+                    <div class="product-top">
+                        <a href="" class="product-thumb">
+                            <img src="img/5.jpg" alt="">
+                        </a>
+                        <a href="" class="mn">Mua ngay</a>
+                    </div>                   
+                    <div class="product-info">
+                        <a href="" class="product-name">City Classic</a>
+                        <div class="gia" >
+                            <div class="price">400.000đ</div>
+                            <div class="priceg">1.400.000đ</div>
+                        </div>
+                    </div>
+                </div>
+            </li>
+            <li>
+                <div class="product-item">
+                    <div class="product-top">
+                        <a href="" class="product-thumb">
+                            <img src="img/6.jpg" alt="">
+                        </a>
+                        <a href="" class="mn">Mua ngay</a>
+                    </div>                   
+                    <div class="product-info">
+                        <a href="" class="product-name">City Classic</a>
+                        <div class="gia" >
+                            <div class="price">400.000đ</div>
+                            <div class="priceg">1.400.000đ</div>
+                        </div>
+                    </div>
+                </div>
+            </li>
+            <li>
+                <div class="product-item">
+                    <div class="product-top">
+                        <a href="" class="product-thumb">
+                            <img src="img/7.jpg" alt="">
+                        </a>
+                        <a href="" class="mn">Mua ngay</a>
+</div>
+<div class="product-info">
+                        <a href="" class="product-name">City Classic</a>
+                        <div class="gia" >
+                            <div class="price">400.000đ</div>
+                            <div class="priceg">1.400.000đ</div>
+                        </div>
+                    </div>
+                </div>
+            </li>
+            <li>
+                <div class="product-item">
+                    <div class="product-top">
+                        <a href="" class="product-thumb">
+                            <img src="img/8.jpg" alt="">
+                        </a>
+                        <a href="" class="mn">Mua ngay</a>
+                    </div>                   
+                    <div class="product-info">
+                        <a href="" class="product-name">City Classic</a>
+                        <div class="gia" >
+                            <div class="price">400.000đ</div>
+                            <div class="priceg">1.400.000đ</div>
+                        </div>
+                    </div>
+                </div>
+            </li>
+            <li>
+                <div class="product-item">
+                    <div class="product-top">
+                        <a href="" class="product-thumb">
+                            <img src="img/9.jpg" alt="">
+                        </a>
+                        <a href="" class="mn">Mua ngay</a>
+                    </div>                   
+                    <div class="product-info">
+                        <a href="" class="product-name">City Classic</a>
+                        <div class="gia" >
+                            <div class="price">400.000đ</div>
+                            <div class="priceg">1.400.000đ</div>
+                        </div>
+                    </div>
+                </div>
+            </li>
+            <li>
+                <div class="product-item">
+                    <div class="product-top">
+                        <a href="" class="product-thumb">
+                            <img src="img/10.jpg" alt="">
+                        </a>
+                        <a href="" class="mn">Mua ngay</a>
+                    </div>                   
+                    <div class="product-info">
+                        <a href="" class="product-name">City Classic</a>
+                        <div class="gia" >
+                            <div class="price">400.000đ</div>
+                            <div class="priceg">1.400.000đ</div>
+                        </div>
+                    </div>
+                </div>
+            </li>
+            <li>
+                <div class="product-item">
+                    <div class="product-top">
+                        <a href="" class="product-thumb">
+                            <img src="img/11.jpg" alt="">
+                        </a>
+                        <a href="" class="mn">Mua ngay</a>
+</div>                   
+                    <div class="product-info">
+<a href="" class="product-name">City Classic</a>
+                        <div class="gia" >
+                            <div class="price">400.000đ</div>
+                            <div class="priceg">1.400.000đ</div>
+                        </div>
+                    </div>
+                </div>
+            </li>
+            <li>
+                <div class="product-item">
+                    <div class="product-top">
+                        <a href="" class="product-thumb">
+                            <img src="img/12.jpg" alt="">
+                        </a>
+                        <a href="" class="mn">Mua ngay</a>
+                    </div>                   
+                    <div class="product-info">
+                        <a href="" class="product-name">City Classic</a>
+                        <div class="gia" >
+                            <div class="price">400.000đ</div>
+                            <div class="priceg">1.400.000đ</div>
+                        </div>
+                    </div>
+                </div>
+            </li>
+                     
+        </ul>
+    </div>
+</body>
